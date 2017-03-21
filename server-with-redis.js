be1ba7d8d9616503d342c7829afb861f83a092ff
Abanoub <abanoub.adelb@gmail.com>
2017-03-21 14:09:40 +0200

    server with redis

--- server-with-redis.js
+++ server-with-redis.js
@@ -2,10 +2,16 @@ var express = require('express')
 var redis = require("redis");
 var app = express();
 
+var client = redis.createClient();
+
 app.get('/', function(req, res) {
-	res.send('Hello World!');
-})
+	client.get("count", function(err, data) {
+		data++;
+		client.set("count", data);
+		res.send(`data: ${data}`);
+	});
+});
 
 app.listen(3000, function() {
 	console.log('Example app listening on port 3000!');
-})
+});
