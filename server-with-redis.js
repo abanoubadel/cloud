418601a02ea2945a543c58ba7555092c874b5f81
Abanoub <abanoub.adelb@gmail.com>
2017-03-21 12:35:30 +0200

    Added some files

---
+++ server-with-redis.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var redis = require("redis");
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('Hello World!');
+})
+
+app.listen(3000, function() {
+	console.log('Example app listening on port 3000!');
+})
